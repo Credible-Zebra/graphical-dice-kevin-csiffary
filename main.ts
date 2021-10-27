e42c2187a80d4ad92c1b9646050108119c9e6e02
Kevin <92882620+Credible-Zebra@users.noreply.github.com>
2021-10-27 06:53:19 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,56 @@
+let side = 0
+input.onGesture(Gesture.Shake, function () {
+    side = randint(0, 5)
+    if (side == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (side == 1) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (side == 2) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (side == 3) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (side == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
